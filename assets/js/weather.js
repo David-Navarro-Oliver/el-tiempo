07899df98cfb3b8619ef465d86013a44e3cac44a
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-28 18:53:33 +0100

    feat(weather): request daily forecast from Open-Meteo

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,6 +1,6 @@
 export async function fetchWeatherData(lat, lon) {
     try {
-        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,weathercode&timezone=auto`);
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`);
         const data = await response.json();
         return data;
     } catch (err) {
