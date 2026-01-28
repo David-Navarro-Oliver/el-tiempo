66ef82a2b9353cac128fb672c8a0e5340a087ab9
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-28 18:07:07 +0100

    feat(location): add geocoding from city name

--- assets/js/location.js
+++ assets/js/location.js
@@ -12,3 +12,19 @@ export async function getCityFromCoordinates(lat, lon) {
     return "Ubicación desconocida";
   }
 }
+export async function getCoordinatesFromCity(city) {
+  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=es&format=json&t=${new Date().getTime()}`;
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    if (result.results[0].latitude && result.results[0].longitude) {
+      const lat = result.results[0].latitude;
+      const lon = result.results[0].longitude;
+      const display_name = result.results[0].name;
+      return { lat, lon, display_name };
+    }
+  } catch (error) {
+    console.error("Error obteniendo las coordenadas de la ciudad" + city, error);
+  }
+}
