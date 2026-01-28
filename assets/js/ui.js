80a7b7abccf10fbc5ff4d8954518df31187d2871
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-28 18:10:24 +0100

    feat(ui): add flash config and DOM references

---
+++ assets/js/ui.js
@@ -0,0 +1,12 @@
+import { getWeatherConfig } from './weather.js';
+const flash = document.getElementById('flash');
+const charImg = document.getElementById('icon');
+
+const flashColors = {
+    'rengoku': '#FF4500',
+    'zenitsu': '#FFFF00',
+    'tomioka': '#00BFFF',
+    'tanjiro': '#00FF7F',
+    'kocho': '#9370DB',
+    'nezuko': '#FF1493'
+};
