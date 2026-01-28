26e22de9b5fa1d61ac5b1d38706c12a968487924
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-28 18:29:12 +0100

    feat(main): initialize app on DOMContentLoaded

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,13 @@ const searchBtn = document.getElementById("search-btn");
 const geoBtn = document.getElementById("geo-btn");
 const charContainer = document.getElementById("char-container");
 let globalWeatherConfig = null;
+document.addEventListener("DOMContentLoaded", async () => {
+  const { lat, lon } = await geolocate();
+
+  const city = await getCityFromCoordinates(lat, lon);
+  const weatherData = await fetchWeatherData(lat, lon);
+  globalWeatherConfig = await getWeatherConfig(
+    weatherData.current_weather.weathercode,
+  );
+  updateDisplay(weatherData, city, globalWeatherConfig);
+});
