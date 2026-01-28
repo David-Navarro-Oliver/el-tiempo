62b452ecb1b3e6cb023bcdb2a423a87c760a6f34
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-28 18:01:13 +0100

    feat(weather): add fetchWeatherData for Open-Meteo

---
+++ assets/js/weather.js
@@ -0,0 +1,9 @@
+export async function fetchWeatherData(lat, lon) {
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,weathercode&timezone=auto`);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error("Error cargando datos:", err);
+    }
+}
